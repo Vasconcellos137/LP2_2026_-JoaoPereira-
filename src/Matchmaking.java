b995d91c1a96f288ff4f08f5187388fd551c90e9
joapedrop <pereira.carlosdaniel@gmail.com>
2026-05-28 09:59:08 -0300

    matchmaking

---
+++ src/Matchmaking.java
@@ -0,0 +1,28 @@
+public class Matchmaking {
+
+    static java.util.Scanner Ler = new java.util.Scanner(System.in);
+
+    public static void main(String[] args) {
+        int escolha  = Ler.nextInt();
+        
+        switch (escolha) {
+            case 1:
+                
+                break;
+
+                case 2:
+                
+                break;
+
+                case 3:
+
+                break;
+
+            case 0:
+            break;
+        
+            default:
+                break;
+        }
+    }
+}
